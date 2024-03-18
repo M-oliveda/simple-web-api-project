6282263731f26bbe4bb0fda64dcc95543284ecd5
Mauricio Oliveda <maurioliveda@gmail.com>
2024-03-18 09:06:10 -0300

    :white_check_mark: Add coverage tests.

---
+++ check-coverage.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+TEST_URL="./src/tests/coverage/coverage-summary.json"
+
+#lines_covered="$(cat $TEST_URL | jq -r ".total.lines.pct")"
+lines_covered=20
+statements_covered="$(cat $TEST_URL | jq -r ".total.statements.pct")"
+functions_covered="$(cat $TEST_URL | jq -r ".total.functions.pct")"
+
+
+if [ "${lines_covered%.*}" -ge 80 -a "${statements_covered%.*}" -ge 80 -a "${functions_covered%.*}" -ge 80 ]
+then
+    echo "The test coverage percentages are good!"
+    return 0;
+else
+    echo "The test coverage percentages are not sufficient."
+    return 1;
+fi
